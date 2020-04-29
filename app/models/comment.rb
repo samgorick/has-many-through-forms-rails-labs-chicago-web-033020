b63e50f8a8016953d3421648d195ba455270619e
gozmeister <samgorick.sg@gmail.com>
2020-04-28 19:45:11 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user_attributes)
+    if !user_attributes["username"].empty?
+      self.user = User.find_or_create_by(user_attributes)
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,20 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      if !category_attribute["name"].empty?
+        category = Category.find_or_create_by(category_attribute)
+        self.categories << category
+      end
+    end
+  end
+
+  def unique_users
+    users = self.comments.map do |comment|
+      comment.user
+    end
+    users.uniq
+  end
 
 end
